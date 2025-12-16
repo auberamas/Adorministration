8ddc0d223d62a9a1cd7fc8ed2dca4364aed2a1f9
auberamas <151127487+auberamas@users.noreply.github.com>
2025-12-16 14:43:05 +0800

    rename files

--- dormitory-management-system/client/src/router/index.js
+++ dormitory-management-system/client/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
-import LoginView from "../views/LoginView.vue";
-import DashboardView from "../views/DashboardView.vue";
-import AdminDashboardView from "../views/AdminDashboardView.vue";
+import Login from "../components/Login.vue";
+import Dashboard from "../components/Dashboard.vue";
+import AdminDashboard from "../components/AdminDashboard.vue";
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", redirect: "/login" },
-    { path: "/login", component: LoginView },
-    { path: "/admin", name: "admin", component: AdminDashboardView },
-    { path: "/dashboard", component: DashboardView }
+    { path: "/login", component: Login },
+    { path: "/admin", name: "admin", component: AdminDashboard },
+    { path: "/dashboard", component: Dashboard }
   ]
 });
